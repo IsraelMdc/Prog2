3f3a17404ff9741cb9145dbe222ed0137bbc4ace
IsraelMdc <israelmcarmo@hotmail.com>
2023-04-21 23:13:14 -0300

    Atividade 17

--- Reforço Prog/atividade17.py
+++ Reforço Prog/atividade17.py
@@ -1,9 +1,15 @@
+
+#Diretório Absoluto de onde o arquivo de Origem(dados a serem processados) está
 caminho = "C:\\Users\\israe\\Desktop\\Federal\\Programação 2\\Prog2\\Prog2\\Reforço Prog\\dadosEntrada.txt"
+#Diretório Absoluto de onde o arquivo de Saída estará. 
+#Obs: o arquivo será salvado com o último nome escrito no diretório e precisa ser diferente para que não sobrescreva arquivos
 caminho_saida = "C:\\Users\\israe\\Desktop\\Federal\\Programação 2\\Prog2\\Prog2\\Reforço Prog\\dadosEntrada_processados.txt"
 
 lista = []
+#Abre o arquivo
 arquivo= open(caminho,"rt")
 
+#Extrai as idades do arquivo de origem
 for idade in arquivo:
     print (idade)
     idade = idade.rstrip('\n') 
@@ -12,9 +18,13 @@ for idade in arquivo:
 lista.sort()
 print (lista)
 
+
+#Mostra no console cada elemento na lista, um embaixo do outro
 for i in range(len(lista)):
     print(lista[i])
 
+
+#Função que cria e armazena os dados da lista em um novo arquivo usando o \n em cada linha
 arquivo_processado = open(f"{caminho_saida}","wt")
 for i in range(len(lista)):
     arquivo_processado.write(str(lista)+ '\n')
